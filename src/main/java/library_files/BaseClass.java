52a24bd6617f9e27e49ba5c6cfd0e11b73711f36
sunny waze <sunnywaze2@gmail.com>
2023-02-22 10:31:02 +0530

    Initial Commit

---
+++ src/main/java/library_files/BaseClass.java
@@ -0,0 +1,11 @@
+package library_files;
+
+public class BaseClass
+{
+	public static void main(String[] args)
+	{
+		UtilityClass UC = new UtilityClass();
+		UC.m1();
+	}
+	
+}
---
+++ src/main/java/library_files/UtilityClass.java
@@ -0,0 +1,12 @@
+package library_files;
+
+public final class UtilityClass
+{
+	
+	
+	public void m1()
+	{
+		System.out.println("fnka");
+	}
+
+}
